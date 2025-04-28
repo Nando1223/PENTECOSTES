3cbf74a2a90101bda63c421e3eaa97dee0d4ed13
Nando1223 <nadelaese@gmail.com>
2025-04-27 20:03:36 -0500

    ss

--- guardar_asistente.php
+++ guardar_asistente.php
@@ -4,6 +4,7 @@ require_once 'conexion.php';
 header('Content-Type: application/json');
 
 try {
+    
     $conn = new PDO("sqlsrv:server=$serverName;Database=$database", $username, $password);
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
